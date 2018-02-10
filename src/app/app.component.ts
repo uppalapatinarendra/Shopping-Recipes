876881c5a61c711c5f68eca47ff0ceba3e728f8e
uppalapatinarendra <uppalapatinarendra.varma@yahoo.com>
2018-02-10 12:12:43 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
+import { AuthService } from './auth/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  loadedFeature = 'recipe';
+
+  constructor(private authService: AuthService){}
+
+  ngOnInit() {
+    firebase.initializeApp({
+      apiKey: "AIzaSyBeW5YMKuyGPSZhF7sGftbc-xk90ZiOoa0",
+    authDomain: "ng-recipe-book-f1cae.firebaseapp.com"
+
+    });
+    firebase.auth().onAuthStateChanged(authData => {
+      if (authData) {
+          console.log("user " + authData.uid + " is logged in with " + authData.providerData);
+          this.authService.getToken();
+      } else {
+          console.log("user is logged out");
+          this.authService.logout();
+      }
+  });
+  }
+
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
+  }
 }
